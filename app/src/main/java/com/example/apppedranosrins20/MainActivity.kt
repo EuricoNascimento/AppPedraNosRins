8ca96543de58f49eeb99b2d46ad61c4ba837de07
João Lucas <santosviegasj@gmail.com>
2021-10-05 08:08:49 -0300

    SplashScreen added

--- app/src/main/java/com/example/apppedranosrins20/MainActivity.kt
+++ app/src/main/java/com/example/apppedranosrins20/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.apppedranosrins20
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.apppedranosrins20.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(){
+
+    private lateinit var binding: ActivityMainBinding
+
+    @SuppressLint("ResourceType")
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_AppPedraNosRins20)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textView.setOnClickListener {
+            Toast.makeText(this,"Seu nome e: Juarez, el cabalo loco", Toast.LENGTH_LONG).show()
+        }
     }
 }
