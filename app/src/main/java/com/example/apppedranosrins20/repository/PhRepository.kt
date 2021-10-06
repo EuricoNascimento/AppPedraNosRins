24800cb40de8108a5a8fd8eb43cceb0ac34bbe9c
Eurico <euriconascimentofn@gmail.com>
2021-10-06 15:37:01 -0300

    Create return

--- app/src/main/java/com/example/apppedranosrins20/repository/PhRepository.kt
+++ app/src/main/java/com/example/apppedranosrins20/repository/PhRepository.kt
@@ -1,4 +1,16 @@
 package com.example.apppedranosrins20.repository
 
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
+import kotlin.random.Random
+
 class PhRepository {
+
+    suspend fun getMedPh(): Double{
+        return withContext(Dispatchers.Default) {
+            delay(3000)
+            Random.nextDouble(4.5, 8.5)
+        }
+    }
 }
